75ad53a1aa5fc102e8ea0e43e9e935e64b738b3e
saqibarfeen <k180960@nu.edu.pk>
2019-01-04 16:19:04 +0500

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,58 @@
+# Class: trace9
+# ===========================
+#
+# Full description of class trace9 here.
+#
+# Parameters
+# ----------
+#
+# Document parameters here.
+#
+# * `sample parameter`
+# Explanation of what this parameter affects and what it defaults to.
+# e.g. "Specify one or more upstream ntp servers as an array."
+#
+# Variables
+# ----------
+#
+# Here you should define a list of variables that this module would require.
+#
+# * `sample variable`
+#  Explanation of how this variable affects the function of this class and if
+#  it has a default. e.g. "The parameter enc_ntp_servers must be set by the
+#  External Node Classifier as a comma separated list of hostnames." (Note,
+#  global variables should be avoided in favor of class parameters as
+#  of Puppet 2.6.)
+#
+# Examples
+# --------
+#
+# @example
+#    class { 'trace9':
+#      servers => [ 'pool.ntp.org', 'ntp.local.company.com' ],
+#    }
+#
+# Authors
+# -------
+#
+# Author Name <author@domain.com>
+#
+# Copyright
+# ---------
+#
+# Copyright 2019 Your name here, unless otherwise noted.
+#
+class trace9 {
+
+file { 
+ '/home/vagrant/scripts':
+  ensure => 'directory',
+  source => 'puppet:///modules/ycsb/scripts',
+  recurse => 'remote',
+  path => '/home/vagrant/YCSB/scripts',
+  owner => 'vagrant',
+  group => 'vagrant',
+  mode  => '0744', # Use 0700 if it is sensitive
+}
+
+}
